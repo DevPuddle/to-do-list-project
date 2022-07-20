612bb142eec838d992ba3eef5caca11d4c58bab8
Isaiah Henson <Devpuddles@gmail.com>
2022-07-20 00:35:22 -0400

    added content to the ejs folder

--- models/todotask.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const todoTaskSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    }
-})
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 //Declare variables
+const { render } = require('ejs');
 const express = require('express')
 const app = express()
 const PORT = 8500;
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const dotenv = require('dotenv').config()
-// add model variable
+const TodoTask = require('./models/todotasks')
 
 //Middleware to help interpret things 
 app.set("view engine", "ejs")
@@ -15,6 +16,16 @@ mongoose.connect(process.env.DB_CONNECTION,
     {useNewUrlParser: true},
     () => {console.log('Connected to database')}
 )
+
+app.get('/', async (req, res) => {
+    try {
+        TodoTask.find({}, (err, tasks) => {
+            res.render('index.ejs', {TodoTasks: tasks})
+        })
+    } catch (err) {
+        if (err) return res.status(500).send(err)
+    }
+})
 
 app.listen(PORT, () => console.log(`Server is running on port 
 ${PORT} great going!`))
