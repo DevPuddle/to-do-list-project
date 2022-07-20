0cc0ea302e10ab12aac220eb494aef7b6faf82a3
Isaiah Henson <Devpuddles@gmail.com>
2022-07-19 22:55:18 -0400

    added database connection

---
+++ server.js
@@ -0,0 +1,23 @@
+//Declare variables
+const express = require('express')
+const app = express()
+const PORT = 8500;
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+// add model variable
+
+//Middleware to help interpret things 
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+mongoose.connect()
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} great going!`)
+})
+
+
+
+
+
